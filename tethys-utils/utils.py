97143ed388a8f47bcabacc80d0cf05887cd77e79
mullenkamp <mullenkamp1@gmail.com>
2020-08-01 19:34:07 +1200

    update

--- tethys-utils/utils.py
+++ tethys-utils/utils.py
@@ -5,11 +5,8 @@ import os
 import io
 import numpy as np
 import zstandard as zstd
-from pdsql import mssql
 import pickle
 import pandas as pd
-from pyproj import Proj, CRS, Transformer
-
 
 #####################################################
 ### Functions
@@ -120,89 +117,6 @@ def read_pkl_zstd(obj, unpickle=True):
     return obj1
 
 
-def get_hydstra_sites(server, database, username=None, password=None):
-    """
-
-    """
-    hy_sites = mssql.rd_sql(server, database, 'SITE', ['STATION', 'STNAME', 'EASTING', 'NORTHING', 'OWNER', 'ORGCODE', 'DATEMOD', 'TIMEMOD'], username=username, password=password)
-    hy_sites.rename(columns={'STATION': 'site', 'STNAME': 'site_name', 'EASTING': 'NZTMX', 'NORTHING': 'NZTMY', 'OWNER': 'owner', 'ORGCODE': 'orgcode', 'DATEMOD': 'mod_date', 'TIMEMOD': 'mod_time'}, inplace=True)
-    hy_sites = hy_sites[hy_sites.site.str.contains('\d+')].copy()
-
-    hy_sites.site = hy_sites.site.str.strip()
-    hy_sites.site_name = hy_sites.site_name.str.strip()
-
-    # fix the stupid combo of mod date and time...
-    hy_sites['mod_time'] = pd.to_datetime(hy_sites.mod_time + 1, format='%H%M', errors='coerce')
-    hy_sites.loc[hy_sites['mod_time'].isnull(), 'mod_time'] = pd.Timestamp('1900-01-01 00:00:00')
-    hy_sites['mod_time'] = pd.to_timedelta(hy_sites['mod_time'].dt.time.astype(str), unit='h')
-    hy_sites['mod_date'] = pd.to_datetime(hy_sites['mod_date'].dt.date) + hy_sites['mod_time']
-    hy_sites.drop('mod_time', axis=1, inplace=True)
-
-    # Remove nonsense sites - Sites with locations in New Zealand
-    hy_sites2 = hy_sites[(hy_sites.NZTMX > 1000000) & (hy_sites.NZTMY > 5000000)].copy()
-
-    # Filter out only ECan sites
-    hy_sites2.owner = hy_sites2.owner.str.lower().str.strip()
-    hy_sites2.orgcode = hy_sites2.orgcode.str.lower().str.strip()
-
-    niwa_sites_bool = (hy_sites2.owner == 'niwa') | (hy_sites2.orgcode == 'n')
-    ecs_sites_bool = (hy_sites2.orgcode == 'ecs') | (hy_sites2.owner == 'ecs')
-    bor_sites_bool = (hy_sites2.orgcode == 'b')
-    ccc_sites_bool = (hy_sites2.orgcode == 'c') | (hy_sites2.owner == 'ccc')
-    private_sites_bool = (hy_sites2.orgcode == 'p')
-
-    hy_sites3 = hy_sites2[~(niwa_sites_bool | ecs_sites_bool | bor_sites_bool | ccc_sites_bool | private_sites_bool)].drop(['owner', 'orgcode'], axis=1).copy()
-
-    from_crs = Proj(CRS.from_user_input(2193))
-    to_crs = Proj(CRS.from_user_input(4326))
-
-    trans1 = Transformer.from_proj(from_crs, to_crs)
-    points1 = trans1.transform(*hy_sites3[['NZTMY', 'NZTMX']].values.T)
-
-    hy_sites3['lon'] = points1[1]
-    hy_sites3['lat'] = points1[0]
-
-    return hy_sites3
-
-
-def get_well_data(server, username, password):
-    """
-
-    """
-    database = 'Wells'
-    well_details_table = 'well_details'
-    well_screen_table = 'screen_details'
-
-    well_details_cols = ['well_no', 'nztmx', 'nztmy', 'ground_rl', 'depth', 'diameter', 'screens']
-    well_screen_cols = ['well_no', 'top_screen', 'bottom_screen']
-
-    well_d1 = mssql.rd_sql(server, database, well_details_table, well_details_cols, username=username, password=password)
-    well_s1 = mssql.rd_sql(server, database, well_screen_table, well_screen_cols, username=username, password=password)
-
-    well_d1['well_no'] = well_d1['well_no'].str.upper().str.strip()
-    well_s1['well_no'] = well_s1['well_no'].str.upper().str.strip()
-
-    well_s_grp = well_s1.groupby('well_no')
-    well_s_max = well_s_grp['bottom_screen'].max()
-    well_s_min = well_s_grp['top_screen'].min()
-    well_s2 = pd.concat([well_s_min, well_s_max], axis=1).reset_index()
-
-    well_d2 = pd.merge(well_d1, well_s2, on='well_no', how='left')
-
-    from_crs = Proj(CRS.from_user_input(2193))
-    to_crs = Proj(CRS.from_user_input(4326))
-
-    trans1 = Transformer.from_proj(from_crs, to_crs)
-    points1 = trans1.transform(*well_d2[['nztmy', 'nztmx']].values.T)
-
-    well_d2['lon'] = np.round(points1[1], 5)
-    well_d2['lat'] = np.round(points1[0], 5)
-
-    well_d2.drop(['nztmx', 'nztmy'], axis=1, inplace=True)
-
-    return well_d2.set_index('well_no')
-
-
 def df_to_xarray(df, nc_type, param_name, attrs, encoding, run_date_key, ancillary_variables=None, compression=False, compress_level=1):
     """
 
@@ -306,3 +220,108 @@ def df_to_xarray(df, nc_type, param_name, attrs, encoding, run_date_key, ancilla
         return c_obj
     else:
         return ds2
+
+
+def grp_ts_agg(df, grp_col, ts_col, freq_code, discrete=False, **kwargs):
+    """
+    Simple function to aggregate time series with dataframes with a single column of sites and a column of times.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Dataframe with a datetime column.
+    grp_col : str or list of str
+        Column name that contains the sites.
+    ts_col : str
+        The column name of the datetime column.
+    freq_code : str
+        The pandas frequency code for the aggregation (e.g. 'M', 'A-JUN').
+    discrete : bool
+        Is the data discrete? Will use proper resampling using linear interpolation.
+
+    Returns
+    -------
+    Pandas resample object
+    """
+
+    df1 = df.copy()
+    if type(df[ts_col].iloc[0]) is pd.Timestamp:
+        df1.set_index(ts_col, inplace=True)
+        if isinstance(grp_col, str):
+            grp_col = [grp_col]
+        else:
+            grp_col = grp_col[:]
+        if discrete:
+            val_cols = [c for c in df1.columns if c not in grp_col]
+            df1[val_cols] = (df1[val_cols] + df1[val_cols].shift(-1))/2
+        grp_col.extend([pd.Grouper(freq=freq_code, **kwargs)])
+        df_grp = df1.groupby(grp_col)
+        return (df_grp)
+    else:
+        print('Make one column a timeseries!')
+
+
+def compare_dfs(old_df, new_df, on):
+    """
+    Function to compare two DataFrames with nans and return a dict with rows that have changed (diff), rows that exist in new_df but not in old_df (new), and rows  that exist in old_df but not in new_df (remove).
+    Both DataFrame must have the same columns.
+
+    Parameters
+    ----------
+    old_df : DataFrame
+        The old DataFrame.
+    new_df : DataFrame
+        The new DataFrame.
+    on : str or list of str
+        The primary key(s) to index/merge the two DataFrames.
+
+    Returns
+    -------
+    dict of DataFrames
+        As described above, keys of 'diff', 'new', and 'remove'.
+    """
+    if ~np.in1d(old_df.columns, new_df.columns).any():
+        raise ValueError('Both DataFrames must have the same columns')
+
+    val_cols = [c for c in old_df.columns if not c in on]
+    all_cols = old_df.columns.tolist()
+
+    comp1 = pd.merge(old_df, new_df, on=on, how='outer', indicator=True, suffixes=('_x', ''))
+
+    rem1 = comp1.loc[comp1._merge == 'left_only', on].copy()
+    add1 = comp1.loc[comp1._merge == 'right_only', all_cols].copy()
+    comp2 = comp1[comp1._merge == 'both'].drop('_merge', axis=1).copy()
+#    comp2[comp2.isnull()] = np.nan
+
+    old_cols = on.copy()
+    old_cols_map = {c: c[:-2] for c in comp2 if '_x' in c}
+    old_cols.extend(old_cols_map.keys())
+    old_set = comp2[old_cols].copy()
+    old_set.rename(columns=old_cols_map, inplace=True)
+    new_set = comp2[all_cols].copy()
+
+    comp_list = []
+    for c in val_cols:
+        isnull1 = new_set[c].isnull()
+        if isnull1.any():
+            new_set.loc[new_set[c].isnull(), c] = np.nan
+        if old_set[c].dtype.name == 'float64':
+            c1 = ~np.isclose(old_set[c], new_set[c])
+        elif old_set[c].dtype.name == 'object':
+            new_set[c] = new_set[c].astype(str)
+            c1 = old_set[c].astype(str) != new_set[c]
+        elif old_set[c].dtype.name == 'geometry':
+            old1 = old_set[c].apply(lambda x: hash(x.wkt))
+            new1 = new_set[c].apply(lambda x: hash(x.wkt))
+            c1 = old1 != new1
+        else:
+            c1 = old_set[c] != new_set[c]
+        notnan1 = old_set[c].notnull() | new_set[c].notnull()
+        c2 = c1 & notnan1
+        comp_list.append(c2)
+    comp_index = pd.concat(comp_list, axis=1).any(1)
+    diff_set = new_set[comp_index].copy()
+
+    dict1 = {'diff': diff_set, 'new': add1, 'remove': rem1}
+
+    return dict1
