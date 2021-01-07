3c641f76dcc58419beb265eac54d6ce010b3a58b
mullenkamp <mullenkamp1@gmail.com>
2021-01-08 09:53:16 +1300

    removed get_altitude and put it into tethysts.utils

--- tethys_utils/altitude_io.py
+++ tethys_utils/altitude_io.py
@@ -3,9 +3,6 @@ Functions to query APIs for altitude data.
 
 """
 import requests
-import orjson
-import zstandard as zstd
-from tethysts import Tethys
 
 ######################################
 #### Parameters
@@ -38,41 +35,6 @@ def koordinates_raster_query(base_url: str, key: str, layer_id: (int, str), lon:
 
     else:
         return resp.content.decode()
-
-
-def get_altitude(stn_df, dataset_id, koordinates_key, tethys_remote=None, missing_value=-9999):
-    """
-
-    """
-    stn_df1 = stn_df[['station_id', 'lon', 'lat']].drop_duplicates(subset=['station_id']).copy()
-
-    if isinstance(tethys_remote, dict):
-        try:
-            t1 = Tethys([tethys_remote])
-            old_stns = t1.get_stations(dataset_id)
-        except:
-            old_stns = []
-    else:
-        old_stns = []
-
-    alt1 = []
-    for i, row in stn_df1.iterrows():
-        # print(row['station_id'])
-        old_stn_alt = [s['altitude'] for s in old_stns if s['station_id'] == row['station_id']]
-
-        if old_stn_alt:
-            alt1.extend(old_stn_alt)
-        else:
-            try:
-                r1 = koordinates_raster_query('https://data.linz.govt.nz', koordinates_key, '51768', row.lon, row.lat)[0]['value']
-                alt1.extend([round(r1, 3)])
-            except:
-                print('No altitude found for ' + row['station_id'] + ', using: ' + str(missing_value))
-                alt1.extend([missing_value])
-
-    stn_df1['altitude'] = alt1
-
-    return stn_df1
 
 
 ####################################
