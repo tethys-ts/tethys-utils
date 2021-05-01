df053f87ff08b79bad71989e59c5ca24a6c5397a
Mike <mullenkamp1gmail.com>
2021-05-01 13:56:56 +1200

    fix imports

--- tethys_utils/__init__.py
+++ tethys_utils/__init__.py
@@ -1,4 +1,1 @@
-from tethys_utils.main import *
-from tethys_utils import data_models
-from tethys_utils import hilltop
-from tethys_utils import altitude_io
+from tethys_utils import data_models, hilltop, altitude_io, misc, processing, s3
